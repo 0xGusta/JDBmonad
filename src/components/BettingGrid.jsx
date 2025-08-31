249a4e78e00fcb3ba16f53eaf96b76e3f6c7020d
0xGusta <160532478+0xGusta@users.noreply.github.com>
2025-08-31 07:12:22 -0400

    Update BettingGrid.jsx

--- src/components/BettingGrid.jsx
+++ src/components/BettingGrid.jsx
@@ -2,22 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { parseEther } from "ethers";
 
 const animals = [
-    { name: "Monlandak", numbers: [0, 1, 2, 3, 4, 5], img: "/images/placeholder.png" },
+    { name: "Monlandak", numbers: [0, 1, 2, 3, 4, 5], img: "/molandak.jpg" },
     { name: "Chog", numbers: [6, 7, 8, 9, 10, 11], img: "/images/chog.jpg" },
-    { name: "Moyaki", numbers: [12, 13, 14, 15, 16, 17], img: "/images/placeholder.png" },
-    { name: "Mouch", numbers: [18, 19, 20, 21, 22, 23], img: "/images/placeholder.png" },
-    { name: "Salmonad", numbers: [24, 25, 26, 27, 28, 29], img: "/images/placeholder.png" },
-    { name: "Moncock", numbers: [30, 31, 32, 33, 34, 35], img: "/images/placeholder.png" },
-    { name: "Snelly", numbers: [36, 37, 38, 39, 40, 41], img: "/images/placeholder.png" },
-    { name: "Salandak", numbers: [42, 43, 44, 45, 46, 47], img: "/images/placeholder.png" },
-    { name: "Honk", numbers: [48, 49, 50, 51, 52, 53], img: "/images/placeholder.png" },
-    { name: "Mokadel", numbers: [54, 55, 56, 57, 58, 59], img: "/images/placeholder.png" },
-    { name: "Lyraffe", numbers: [60, 61, 62, 63, 64, 65], img: "/images/placeholder.png" },
-    { name: "Spidermon", numbers: [66, 67, 68, 69, 70, 71], img: "/images/placeholder.png" },
-    { name: "Montiger", numbers: [72, 73, 74, 75, 76, 77], img: "/images/placeholder.png" },
-    { name: "Moxy", numbers: [78, 79, 80, 81, 82, 83], img: "/images/placeholder.png" },
-    { name: "Birbie", numbers: [84, 85, 86, 87, 88, 89], img: "/images/placeholder.png" },
-    { name: "MonCoringa", numbers: [90, 91, 92, 93, 94, 95], img: "/images/placeholder.png" },
+    { name: "Moyaki", numbers: [12, 13, 14, 15, 16, 17], img: "/images/moyaki.jpg" },
+    { name: "Mouch", numbers: [18, 19, 20, 21, 22, 23], img: "/images/mouch.jpg" },
+    { name: "Salmonad", numbers: [24, 25, 26, 27, 28, 29], img: "/images/salmonad.jpg" },
+    { name: "Moncock", numbers: [30, 31, 32, 33, 34, 35], img: "/images/moncock.jpg" },
+    { name: "Snelly", numbers: [36, 37, 38, 39, 40, 41], img: "/images/snelly.jpg" },
+    { name: "Salandak", numbers: [42, 43, 44, 45, 46, 47], img: "/images/salandak.jpg" },
+    { name: "Honk", numbers: [48, 49, 50, 51, 52, 53], img: "/images/honk.jpg" },
+    { name: "Mokadel", numbers: [54, 55, 56, 57, 58, 59], img: "/images/mokadel.jpg" },
+    { name: "Lyraffe", numbers: [60, 61, 62, 63, 64, 65], img: "/images/lyraffe.jpg" },
+    { name: "Spidermon", numbers: [66, 67, 68, 69, 70, 71], img: "/images/spidermon.jpg" },
+    { name: "Montiger", numbers: [72, 73, 74, 75, 76, 77], img: "/images/montiger.jpg" },
+    { name: "Moxy", numbers: [78, 79, 80, 81, 82, 83], img: "/images/moxy.jpg" },
+    { name: "Birbie", numbers: [84, 85, 86, 87, 88, 89], img: "/images/birbie.jpg" },
+    { name: "Monavara", numbers: [90, 91, 92, 93, 94, 95], img: "/images/monavara.jpg" },
 ];
 
 
