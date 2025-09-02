720a716199557f768241ea8c3b9b1338c82c8349
0xGusta <160532478+0xGusta@users.noreply.github.com>
2025-09-02 09:39:33 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,8 +27,17 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="social-link">
-                    <a href={XLink} target="_blank" rel="noopener noreferrer">
+                    <a href={XLink} target="_blank" rel="noopener noreferrer" style={{ display: 'none' }}>
                         <img src={xLogoUrl} alt="X Logo" />
+                    </a>
+                    <a href="https://entropy-explorer.pyth.network/?chain=monad-testnet&search=0x84efFC21412947c12B4fa37016cD5d535b5c9CB0" target="_blank" rel="noopener noreferrer">
+                        <img src="/images/pyth-network.svg" alt="Pyth Network Entropy Explorer" />
+                    </a>
+                    <a href="https://github.com/0xGusta/JDBmonad" target="_blank" rel="noopener noreferrer">
+                        <img src="/images/github-mark-white.svg" alt="GitHub Logo" />
+                    </a>
+                    <a href="https://testnet.monadexplorer.com/address/0x84efFC21412947c12B4fa37016cD5d535b5c9CB0?tab=Contract" target="_blank" rel="noopener noreferrer">
+                        <img src="/images/monvision.svg" alt="Monvision Logo" />
                     </a>
                 </div>
             </div>
